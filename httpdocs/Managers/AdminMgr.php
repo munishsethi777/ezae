e772160ddde57dffba2580b1d324826c8d9749f8
munishsethi777 <munishsethi777@gmail.com>
2014-09-06 23:54:23 +0530

    managers

---
+++ httpdocs/Managers/AdminMgr.php
@@ -0,0 +1,13 @@
+<?php
+require_once($ConstantsArray['dbServerUrl']. "DataStores\\AdminDataStore.php5");
+      
+class AdminMgr{
+      public function logInAdmin($username, $password){
+            $adminDataStore = AdminDataStore::getInstance();
+            $admin = $adminDataStore::findByUserName($userName);
+            if($admin == null){
+                return "No User Found";
+            }
+      }
+}
+?>
---
+++ httpdocs/Managers/UserMgr.php
@@ -0,0 +1,3 @@
+<?php
+      required_once("DataStores/");
+?>
