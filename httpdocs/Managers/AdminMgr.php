a153c0cf35fe995bc7e79e0b7ae66f210d3ff63c
munishsethi777 <munishsethi777@gmail.com>
2014-09-07 07:43:55 +0530

    adminmgr

--- httpdocs/Managers/AdminMgr.php
+++ httpdocs/Managers/AdminMgr.php
@@ -4,7 +4,7 @@ require_once($ConstantsArray['dbServerUrl']. "DataStores\\AdminDataStore.php5");
 class AdminMgr{
       public function logInAdmin($username, $password){
             $adminDataStore = AdminDataStore::getInstance();
-            $admin = $adminDataStore::findByUserName($userName);
+            $admin = $adminDataStore->findByUserName($userName);
             if($admin == null){
                 return "No User Found";
             }
