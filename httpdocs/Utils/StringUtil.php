b8d1f39629cbc05f3fc741aa3080daf24d5e0806
bgaheer <baljeetgaheer@gmail.com>
2015-03-21 14:12:20 +0530

    utils

---
+++ httpdocs/Utils/StringUtil.php
@@ -0,0 +1,14 @@
+<?php
+  class StringUtil{
+      
+      public static function IsNullOrEmptyString($str){
+        return (!isset($str) || trim($str)==='');
+      }
+      
+      public static function generatePassword( $length = 8 ) {
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
+        $password = substr( str_shuffle( $chars ), 0, $length );
+        return $password;
+      }
+  }
+?>
