044ebe17ad466ae89963abdd4f51bf7c98f78554
bgaheer <baljeetgaheer@gmail.com>
2015-03-29 17:45:36 +0530

    Manager

---
+++ httpdocs/Managers/LearningProfileMgr.php
@@ -0,0 +1,59 @@
+<?php
+    require_once($ConstantsArray['dbServerUrl']. "DataStores/BeanDataStore.php5");
+    require_once($ConstantsArray['dbServerUrl']. "BusinessObjects/tag.php");
+    class LearningProfileMgr{
+        private static $learnerProfileMgr;
+        private static $dataStore;   
+        public static function getInstance()
+        {
+            if (!self::$learnerProfileMgr)
+            {
+                self::$learnerProfileMgr = new LearningProfileMgr();
+                self::$dataStore = new BeanDataStore("tag","learningprofiles");
+                return self::$learnerProfileMgr;
+            }
+            return self::$learnerProfileMgr;
+        }  
+        
+        public function getLearnerProfilesForGrid($adminSeq,$companySeq){
+            $fullArr = array();
+            $params = array();
+            $params["adminseq"] = $adminSeq;
+            $params["companyseq"] = $companySeq; 
+            $learnerProfiles = self::$dataStore->executeConditionQuery($params);
+            foreach($learnerProfiles as $lpObj){
+                $lp = new tag();
+                $lp = $lpObj;
+                $arr = array();
+                $arr['id'] = $lp->getSeq();
+                $arr['tag'] = $lp->getTag();
+                $arr['description'] = $lp->getDescription();
+                array_push($fullArr,$arr);
+            }
+            return json_encode($fullArr);    
+        }
+        
+        public function deleteCustomFields($ids){
+            self::$dataStore->deleteInList($ids);
+        }
+        
+        public function Save($learningProfile,$isAjaxCall = false){
+            $id = self::$dataStore->save($learningProfile);
+            if($isAjaxCall){
+                $learningProfile->setSeq($id);
+                return $this->getSavedRowArray($learningProfile);    
+            }
+            return $id;
+        }
+        
+        private function getSavedRowArray($learningProfile){
+            $learningProfileObj = new tag();
+            $learningProfileObj = $learningProfile;
+            $row = array();
+            $row["id"] = $learningProfileObj->getSeq();
+            $row["tag"] = $learningProfileObj->getTag();
+            $row["description"] = $learningProfileObj->getDescription();
+            return json_encode($row);
+        } 
+    }
+?>
