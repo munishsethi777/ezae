741f863a50ecbac9ba5154b0e57f568533a2791c
bgaheer <baljeetgaheer@gmail.com>
2015-06-09 16:02:54 +0530

    object

---
+++ httpdocs/BusinessObjects/LearningPlanProfile.php
@@ -0,0 +1,26 @@
+<?php
+    class LearningPlanProfile{
+        private $seq,$learningplanseq,$learningprofileseq;
+
+        public function setSeq($seq_){
+            $this->seq = $seq_;
+        }
+        public function getSeq(){
+            return $this->seq;
+        }
+        public function setLearningPlanSeq($learningPlanSeq_){
+            $this->learningplanseq = $learningPlanSeq_;
+        }
+        public function getLearningPlanSeq(){
+            return $this->learningplanseq;
+        }
+        
+        public function setLearningProfileSeq($learningProfileSeq_){
+            $this->learningprofileseq = $learningProfileSeq_;
+        }
+        public function getLearningProfileSeq(){
+            return $this->learningprofileseq;
+        }
+
+    }
+?>
