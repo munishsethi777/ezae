436ea212546c3146808b211d3885280161573678
bgaheer <baljeetgaheer@gmail.com>
2015-08-04 13:05:06 +0530

    string constants

---
+++ httpdocs/StringConstants.php
@@ -0,0 +1,6 @@
+<?php
+  class StringConstants{
+      const MAIL_MESSAGE_SAVED = "Mail Message Saved Successfully.";
+      const MAIL_MESSAGE_DUPLICATE = "Duplicate Mail Message. Mail Message is already saved with same Module/LearningPlan";
+  }
+?>
