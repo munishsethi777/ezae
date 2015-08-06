b4b8c8352b2dd9fb03a0f67a0256d18d34b6b2e4
bgaheer <baljeetgaheer@gmail.com>
2015-08-06 17:29:03 +0530

    n

--- httpdocs/Modules/demo_new/story_content/user.js
+++ httpdocs/Modules/demo_new/story_content/user.js
@@ -14,11 +14,13 @@ function Script1()
     var moduleId= "1";
     var progress= player.GetVar("Progress");
     var score=player.GetVar("Score");
+
     var learningPlan = parent.learningPlanSeq;
     saveScores(moduleId, progress, score, learningPlan);
+
 }
 function saveScores(moduleId, progress, score,learningPlanSeq){
-    var url = "http://localhost:8080/Actions/ActivityAction.php?call=saveActivityData&moduleId="+moduleId+"&progress="+progress+"&score="+score +"&lpid="+learningPlanSeq;
+    var url = "http://localhost:8083/httpdocs/Actions/ActivityAction.php?call=saveActivityData&moduleId="+moduleId+"&progress="+progress+"&score="+score +"&lpid="+learningPlanSeq;
     $.get(url, function(data){
         //alert(data);
     });
