b1107c8896691190aa9a46c9fa43e7b26580be5a
bgaheer <baljeetgaheer@gmail.com>
2015-08-20 09:19:05 +0530

    scripts

---
+++ database/deleteSqls.sql
@@ -0,0 +1,27 @@
+delete from activities;
+delete from adminmodules;
+delete from signupformfields;
+delete from learningplans;
+delete from userlearningprofiles;
+delete from learningprofiles;
+delete from usercustomfields;
+delete from users;
+delete from admins;
+delete from companies;
+delete from companymodules;
+delete from leaderboard;
+delete from learningplanmodules;
+delete from learningplanprofiles;
+delete from mailmessage;
+delete from mailmessageaction;
+delete from mailmessagelearningprofiles;
+delete from mailmessagemails;
+delete from managercriteria;
+delete from managers;
+delete from matchingrules;
+delete from my_log;
+
+
+
+
+
