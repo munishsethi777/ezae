54d8f17e3bcf569720a365dc4181739558a19878
munishsethi777 <munishsethi777@gmail.com>
2015-09-28 12:24:30 +0530

    ui changes

--- httpdocs/userTrainingCustomModule.php
+++ httpdocs/userTrainingCustomModule.php
@@ -8,8 +8,8 @@
 <?include "ScriptsInclude.php"?>
 <?
     require_once($ConstantsArray['dbServerUrl'] ."Managers/ModuleMgr.php");
-    $learningPlanSeq = 3;//$_GET['lpid'];
-    $moduleId = 2;//$_GET['id'];
+    $learningPlanSeq = 11;//$_GET['lpid'];
+    $moduleId = 5;//$_GET['id'];
     $moduleMgr = ModuleMgr::getInstance();
     $module = $moduleMgr->getModule($moduleId);
     $moduleQuestions = $moduleMgr->getQuestions($moduleId);
@@ -22,22 +22,25 @@
 <div id="wrapper">
         <?include("userMenu.php");?>
         <div class="row">
-            <div class="col-lg-12">
-                <div class="wrapper wrapper-content animated fadeInUp">
+            <div class="col-md-12">
+                <div class="wrapper wrapper-content">
                     <div class="ibox">
                         <div class="ibox-title">
                             <h5><?echo $module->getTitle()?></h5><br><br>
                             <div><?echo $module->getDescription()?></div>
                         </div>
-                         <div id="main-slider" class="liquid-slider">
+                         <div id="main-slider" class="liquid-slider ibox-content">
                              <?$totalCount = count($moduleQuestions);
                              $i = 1;
                              foreach($moduleQuestions as $question){
                                     $questionSeq = $question->getSeq();?>
-                             <div>
-                                 <div class="ibox float-e-margins p-xl col-sm-offset-2 col-sm-20" style="border:silver solid 1px;margin-bottom:20px;">               
-                                    <span class="pull-left">Question <?echo $i?> of <?echo $totalCount?></span>
-                                    <span class="pull-right">Marks:<?echo $question->getMaxMarks()?></span>
+                             <div style="width:100%">
+                             <div class="col-md-offset-1 col-md-10">
+                                 <div  style="border:silver solid 1px;height:500px">
+                                    <div class="ibox-content">
+                                        <span class="pull-left">Question <?echo $i?> of <?echo $totalCount?></span>
+                                        <span class="pull-right">Marks:<?echo $question->getMaxMarks()?></span>
+                                    </div>
                                     <div class="ibox-content ibox-heading">
                                         <h2 style="padding-bottom:10px;">Question <?echo $i?>. <?echo $question->getTitle()?> ?</h2>
                                     </div>
@@ -46,7 +49,7 @@
                                         <input type="hidden" name="questionseq" value="<?echo $questionSeq?>"/>
                                         <input type="hidden" name="moduleseq" value="<?echo $module->getSeq()?>"/>
                                         <input type="hidden" name="learningplanseq" value="<?echo $module->getSeq()?>"/>
-                                        
+
                                         <table class="table table-hover table-mail">
                                             <tbody>
                                             <?$possibleAns = $questionPossibleAns[$question->getSeq()];
@@ -61,30 +64,33 @@
                                                     <td width="90%"><?echo $ans->getTitle()?></td>
                                                     <td><i id="check<?echo $ans->getSeq()?>" class="fa fa-check text-navy" style="display: none;"></i></td>
                                                 </tr>
-                                             <?}?>   
+                                             <?}?>
                                             </tbody>
                                          </table>
-                                         <div class="col-sm-offset-5">
-                                            <button class="btn btn-primary" id="submitBtnDiv<?echo $questionSeq?>" onclick="submitAns(<?echo $questionSeq?>,this)" type="submit"><strong><i class="fa fa-lock"></i> <span class="ladda-label">Submit Answer</strong></span></button>
-                                            
+                                         <div class="p-xl">
+                                             <div class="col-sm-offset-5">
+                                                <button class="btn btn-primary" id="submitBtnDiv<?echo $questionSeq?>" onclick="submitAns(<?echo $questionSeq?>,this)" type="submit"><strong><i class="fa fa-lock"></i> <span class="ladda-label">Submit Answer</strong></span></button>
+
+                                            </div>
+                                            <div id="success<?echo $questionSeq?>" class="alert alert-success" style="display: none;">
+
+                                            </div>
+                                            <div id="danger<?echo $questionSeq?>" class="alert alert-danger" style="display: none;">
+
+                                            </div>
+                                            <button class="btn btn-sm btn-primary m-t-n-xs pull-left" onclick="setPrevPannel(this)" id="prevBtnDiv<?echo $questionSeq?>" style="display: none;" type="button"><strong><i class="fa fa-long-arrow-left"></i> Previous</strong></button>
+                                            <button class="btn btn-sm btn-primary m-t-n-xs pull-right" onclick="setNextPannel(this)" id="nextBtnDiv<?echo $questionSeq?>" style="display: none;" type="button"><strong>Next <i class="fa fa-long-arrow-right"></i></strong></button>
                                         </div>
-                                        <div id="success<?echo $questionSeq?>" class="alert alert-success" style="display: none;">
-                                               
-                                        </div>
-                                        <div id="danger<?echo $questionSeq?>" class="alert alert-danger" style="display: none;">
-                                               
-                                        </div>
-                                        <button class="btn btn-sm btn-primary m-t-n-xs pull-left" onclick="setPrevPannel(this)" id="prevBtnDiv<?echo $questionSeq?>" style="display: none;" type="button"><strong><i class="fa fa-long-arrow-left"></i> Previous</strong></button>
-                                        <button class="btn btn-sm btn-primary m-t-n-xs pull-right" onclick="setNextPannel(this)" id="nextBtnDiv<?echo $questionSeq?>" style="display: none;" type="button"><strong>Next <i class="fa fa-long-arrow-right"></i></strong></button>
                                     </form>
                                 </div>
                              </div>
+                             </div>
                              <?$i++;}?>
-                        </div>              
-             
+                        </div>
+
                    </div>
             </div>
-        </div> 
+        </div>
 
 
 </div>
@@ -93,21 +99,21 @@
 <script>
 
 var api = null;
-$slideCounter = 0;  
-$(document).ready(function(){    
+$slideCounter = 0;
+$(document).ready(function(){
     $('#main-slider').liquidSlider();
     $(".liquid-slider .panel-container .clone form").attr("id","");
     api = $.data( $('#main-slider')[0], 'liquidSlider');
-     
+
     $("#main-slider-nav-ul").hide();
 });
 function setNextPannel(btn){
     $slideCounter++;
-    api.setNextPanel($slideCounter);api.updateClass($(btn));            
+    api.setNextPanel($slideCounter);api.updateClass($(btn));
 }
 function setPrevPannel(btn){
     $slideCounter--;
-    api.setNextPanel($slideCounter);api.updateClass($(btn));            
+    api.setNextPanel($slideCounter);api.updateClass($(btn));
 }
 function submitAns(quesSeq,btn){
     var l = Ladda.create(btn);
@@ -121,8 +127,8 @@ function submitAns(quesSeq,btn){
         anslist = obj.ansList.correct;
         if(obj.success == 0){
             divClass = "#form" + quesSeq + " #danger" + quesSeq;
-            anslist =  obj.ansList.incorrect;    
-        }       
+            anslist =  obj.ansList.incorrect;
+        }
         htmlCont = "";
         $.each(anslist, function(key, value){
            htmlCont = value + "<br/>";
@@ -132,18 +138,18 @@ function submitAns(quesSeq,btn){
         })
         $(divClass).html(htmlCont);
         $(divClass).show();
-        var totalCount = <?echo $totalCount?>
+        var totalCount = <?echo $totalCount?>;
         //$("next");
         if($slideCounter > 0){
-            $("#form" + quesSeq + " #prevBtnDiv" + quesSeq).show();    
-        }        
+            $("#form" + quesSeq + " #prevBtnDiv" + quesSeq).show();
+        }
         if((totalCount -1) != $slideCounter ){
             $("#form" + quesSeq + " #nextBtnDiv" + quesSeq).show();
         }
         $("#form" + quesSeq + " #submitBtnDiv" + quesSeq).hide();
     })
-    
-    
+
+
 }
 
 
